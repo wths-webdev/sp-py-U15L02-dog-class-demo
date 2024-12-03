384330a25354ca15007a68e228df632df5034170
sp1dev <spaulino.dev+github@gmail.com>
2024-12-03 14:10:38 -0500

    separated solution and starter file

---
+++ starter.py
@@ -0,0 +1,24 @@
+# This is our dog:
+
+    # Constructor attributes of a Dog
+    # self means the object we're creating
+
+
+    # behaviors of a Dog (methods)
+
+
+
+
+# Creating objects happens OUTSIDE of the class
+# Create a dog
+
+
+# make rosie bark
+
+
+# update its age
+
+
+
+
+# what if we made a hasBirthday() method?
